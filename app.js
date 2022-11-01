16c6ad21674c4298d85bc2b63635dd689f90d772
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2022-11-01 12:48:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ let _html = {}
 let _request = {}
 let _AUTH = ''
 
-_html.login =
-  '<h1>Login form</h1><hr/>' +
+_html.login = '<h1>Login form</h1><hr/>' +
   "<label for='username'>Fill your username</label><br/>" +
   "<input id='username' type='text'/><br/>" +
   "<label for='password'>Your password</label><br/>" +
   "<input id='password' type='password'/><br/>" +
-  "<button id='submitLogin'>LOGIN</button><br/><br/>" +
+  "<button id='submitLogin' onclick='goToLogin();'>LOGIN</button><br/><br/>" +
   '<span>OR</span><br/>' +
-  "<button id='registration' onclick='goToRegistration();'>Registration form</button>"
+  "<button id='registration' onclick='goToRegistration();'>Registration form</button>";
 
-_html.registration =
-  '<h1>Registration form</h1><hr/>' +
+_html.logout = "<h1>You are logged out!</h1>" + _html.login;
+
+_html.registration = '<h1>Registration form</h1><hr/>' +
   "<label for='username'>Fill your username</label><br/>" +
   "<input id='username' type='text'/><br/>" +
   "<label for='password'>Your password</label><br/>" +
@@ -22,9 +22,15 @@ _html.registration =
   "<input id='confirm-password' type='password'/><br/>" +
   "<button id='submitRegistration'>REGISTRATION</button><br/><br/>" +
   '<span>OR</span><br/>' +
-  "<button id='login' onclick='goToLogin();'>Login form</button>"
+  "<button id='login' onclick='goToLogin();'>Login form</button>";
 
-_html.dashboard = '<h1>Dashboard</h1><hr/>'
+_html.dashboard = '<h1>Dashboard</h1><hr/>' +
+"<table><tr><td>User name: </td><td id='username'>John Doe</td></tr>" + 
+"<tr><td>User nik: </td><td id='usernik'>JD</td></tr>" +
+"<tr><td>User ID: </td><td id='userid'>0</td></tr>" +
+"</table><br>" + 
+'<br>' +
+"<button id='login' onclick='goLogout();'>Log out</button>"; 
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('App js and DOM is ready!')
@@ -48,6 +54,10 @@ function goToLogin () {
   _appendDOM(_html.login)
 }
 
+function goLogout () {
+  _appendDOM(_html.logout)
+}
+
 function _appendDOM (_obj) {
   document.getElementsByTagName('body')[0].innerHTML = _obj
 }
@@ -64,9 +74,28 @@ _request.login = function (_this) {
   }).then(function (response) {
     // _AUTH = response.body.toString();
     // _AUTH = "HASH-dchsdfukxyegnfxaueygnfuiwaF";
-    _appendDOM(_html.dashboard)
-    _request.getUser()
+    //  Добавляем в ДОМ шаблон страницы для отображения данных пользователя
+    _appendDOM(_html.dashboard);
+    // Запрашиваем из сети данные о пользователе и сохраняем их в локальном хранилище
+    _request.getUser();
+    // Отображаем данные пользователя из локального хранилища
+    _request.dashboard();
   })
+}
+
+_request.dashboard = function (_this) {
+  // Запрашиваем из локального хранилища данные о пользователе
+  let userInfo = JSON.parse(localStorage.getItem('userDeatils'));
+  // Отображаем данные пользователя на Dashboard пользователя или заглушки (если нужны)
+  if (Object.keys(userInfo).length != 0) {
+    document.getElementById('userid').innerText = userInfo.id;
+    document.getElementById('username').innerText = userInfo.name;
+    document.getElementById('usernik').innerText = userInfo.username;
+  } else {
+    document.getElementById('userid').innerText = "[Used ID]";
+    document.getElementById('username').innerText = "[User name]";
+    document.getElementById('usernik').innerText = "[User NIK]";
+  }
 }
 
 _request.getUser = function (_this) {
@@ -77,44 +106,37 @@ _request.getUser = function (_this) {
     method: 'GET',
     headers: _headers
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`)
-      }
-      return response
-    })
-    .then(response =>
-      response
-        .json()
-        .then(data => ({
-          data: data,
-          status: response.status
-        }))
-        .then(res => {
-          // Сохраняем информацию о пользователе в localStorage
-          localStorage.setItem('userDeatils', JSON.stringify(res.data))
-          // Отображаем, сохраненную в localStorage информацию
-          console.log(
-            'Saved UserInfo (in the localStorage): ',
-            JSON.parse(localStorage.getItem('userDeatils'))
-          )
-        })
-    )
-    .catch(err => console.error(`Fetch problem: ${err.message}`))
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`)
+    }
+    return response
+  })
+  .then(response =>
+    response
+      .json()
+      .then(data => ({
+        data: data,
+        status: response.status
+      }))
+      .then(res => {
+        // Сохраняем информацию о пользователе в localStorage
+        saveToLS('userDeatils', JSON.stringify(res.data));
+        // localStorage.setItem('userDeatils', JSON.stringify(res.data));
+        // Отображаем, сохраненную в localStorage информацию
+        console.log('Saved UserInfo (in the localStorage): ',
+          // JSON.parse(getFromLS('userDeatils')) // Почему-то приводит к ошибке!???
+          JSON.parse(localStorage.getItem('userDeatils'))
+        )
+      })
+  )
+  .catch(err => console.error(`Fetch problem: ${err.message}`));
 }
 
-function saveToLS (_key, _value) {
-  localStorage.setItem(_key, _value)
+function saveToLS(_key, _value) {
+  localStorage.setItem(_key, _value);
 }
 
-function getFromLS (_key) {
-  localStorage.getItem(_key)
-}
-
-function saveUserIdToLS (userId) {
-  localStorage.setItem('userid', userId)
-}
-
-function getUserIdFromLS () {
-  localStorage.getItem('userid')
+function getFromLS(_key) {
+  localStorage.getItem(_key);
 }
