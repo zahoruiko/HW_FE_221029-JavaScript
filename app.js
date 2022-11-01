94bd313f5ab50bc63c00685f22d2888e017a94a1
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2022-11-01 17:47:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-let _html = {}
-let _request = {}
-let _AUTH = ''
+let _html = {};
+let _request = {};
+let _AUTH = "";
 
-_html.login = '<h1>Login form</h1><hr/>' +
+_html.login =
+  "<h1>Login form</h1><hr/>" +
   "<label for='username'>Fill your username</label><br/>" +
   "<input id='username' type='text'/><br/>" +
   "<label for='password'>Your password</label><br/>" +
   "<input id='password' type='password'/><br/>" +
   "<button id='submitLogin' onclick='goToLogin();'>LOGIN</button><br/><br/>" +
-  '<span>OR</span><br/>' +
+  "<span>OR</span><br/>" +
   "<button id='registration' onclick='goToRegistration();'>Registration form</button>";
 
-_html.logout = "<h1>You are logged out!</h1>" + _html.login;
+_html.logout = "<h2>You are logged out!</h2>" + _html.login;
 
-_html.registration = '<h1>Registration form</h1><hr/>' +
+_html.registration =
+  "<h1>Registration form</h1><hr/>" +
   "<label for='username'>Fill your username</label><br/>" +
   "<input id='username' type='text'/><br/>" +
   "<label for='password'>Your password</label><br/>" +
@@ -21,104 +23,107 @@ _html.registration = '<h1>Registration form</h1><hr/>' +
   "<label for='confirm-password'>Confirm password</label><br/>" +
   "<input id='confirm-password' type='password'/><br/>" +
   "<button id='submitRegistration'>REGISTRATION</button><br/><br/>" +
-  '<span>OR</span><br/>' +
+  "<span>OR</span><br/>" +
   "<button id='login' onclick='goToLogin();'>Login form</button>";
 
-_html.dashboard = '<h1>Dashboard</h1><hr/>' +
-"<table><tr><td>User name: </td><td id='username'>John Doe</td></tr>" + 
-"<tr><td>User nik: </td><td id='usernik'>JD</td></tr>" +
-"<tr><td>User ID: </td><td id='userid'>0</td></tr>" +
-"</table><br>" + 
-'<br>' +
-"<button id='login' onclick='goLogout();'>Log out</button>"; 
+_html.dashboard =
+  "<h1>Dashboard</h1><hr/>" +
+  "<table><tr><td>User name: </td><td id='username'>John Doe</td></tr>" +
+  "<tr><td>User nik: </td><td id='usernik'>JD</td></tr>" +
+  "<tr><td>User ID: </td><td id='userid'>0</td></tr>" +
+  "</table><br>" +
+  "<br>" +
+  "<button id='login' onclick='goLogout();'>Log out</button>";
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('App js and DOM is ready!')
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("App js and DOM is ready!");
   /**
    * Start logic
    */
-  _appendDOM(_html.login)
+  _appendDOM(_html.login);
 
   document
-    .getElementById('submitLogin')
-    .addEventListener('click', function (_el) {
-      _request.login(_el)
-    })
-})
+    .getElementById("submitLogin")
+    .addEventListener("click", function (_el) {
+      _request.login(_el);
+    });
+});
 
-function goToRegistration () {
-  _appendDOM(_html.registration)
+function goToRegistration() {
+  _appendDOM(_html.registration);
 }
 
-function goToLogin () {
-  _appendDOM(_html.login)
+function goToLogin() {
+  _appendDOM(_html.login);
 }
 
-function goLogout () {
-  _appendDOM(_html.logout)
+function goLogout() {
+  _appendDOM(_html.logout);
 }
 
-function _appendDOM (_obj) {
-  document.getElementsByTagName('body')[0].innerHTML = _obj
+function _appendDOM(_obj) {
+  document.getElementsByTagName("body")[0].innerHTML = _obj;
 }
 
 _request.login = function (_this) {
   let _postObj = {
-    username: document.getElementById('username').value,
-    password: document.getElementById('password').value
-  }
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify(_postObj)
-  }).then(function (response) {
-    // _AUTH = response.body.toString();
-    // _AUTH = "HASH-dchsdfukxyegnfxaueygnfuiwaF";
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(_postObj),
+  }).then(function () {
+    // _AUTH = response.body.toString()
+    // _AUTH = "HASH-dchsdfukxyegnfxaueygnfuiwaF"
     //  Добавляем в ДОМ шаблон страницы для отображения данных пользователя
     _appendDOM(_html.dashboard);
     // Запрашиваем из сети данные о пользователе и сохраняем их в локальном хранилище
     _request.getUser();
     // Отображаем данные пользователя из локального хранилища
     _request.dashboard();
-  })
-}
+  });
+};
 
 _request.getUser = function (_this) {
   let _headers = {
     // "AUTH": _AUTH
-  }
-  fetch('http://jsonplaceholder.typicode.com/users/1', {
-    method: 'GET',
-    headers: _headers
+  };
+  fetch("http://jsonplaceholder.typicode.com/users/1", {
+    method: "GET",
+    headers: _headers,
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`)
-    }
-    return response
-  })
-  .then(response =>
-    response
-      .json()
-      .then(data => ({
-        data: data,
-        status: response.status
-      }))
-      .then(res => {
-        // Сохраняем информацию о пользователе в localStorage
-        saveToLS ('userDeatils', JSON.stringify(res.data));
-        // localStorage.setItem('userDeatils', JSON.stringify(res.data));
-        // Отображаем, сохраненную в localStorage информацию
-        console.log('Saved UserInfo (in the localStorage): ',
-          // JSON.parse(getFromLS('userDeatils')) // Почему-то приводит к ошибке!???
-          JSON.parse(localStorage.getItem('userDeatils')))
-      })
-  )
-  .catch(err => console.error(`Fetch problem: ${err.message}`));
-}
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response;
+    })
+    .then((response) =>
+      response
+        .json()
+        .then((data) => ({
+          data: data,
+          status: response.status,
+        }))
+        .then((res) => {
+          // Сохраняем информацию о пользователе в localStorage
+          saveToLS("userDeatils", JSON.stringify(res.data));
+          // localStorage.setItem('userDeatils', JSON.stringify(res.data))
+          // Отображаем, сохраненную в localStorage информацию
+          console.log(
+            "Saved UserInfo (in the localStorage): ",
+            // JSON.parse(getFromLS('userDeatils')) // Почему-то приводит к ошибке!???
+            JSON.parse(localStorage.getItem("userDeatils"))
+          );
+        })
+    )
+    .catch((err) => console.error(`Fetch problem: ${err.message}`));
+};
 
 _request.dashboard = function (_this) {
   // Запрашиваем из локального хранилища данные о пользователе
-  let userInfo = JSON.parse(localStorage.getItem('userDeatils'));
+  let userInfo = JSON.parse(localStorage.getItem("userDeatils"));
   // Отображаем данные пользователя на Dashboard пользователя или заглушки (если нужны)
   let id, name, nik;
   if (Object.keys(userInfo).length != 0) {
@@ -130,10 +135,10 @@ _request.dashboard = function (_this) {
     name = "[User name]";
     nik = "[User NIK]";
   }
-  document.getElementById('userid').innerText = id;
-  document.getElementById('username').innerText = name;
-  document.getElementById('usernik').innerText = nik;
-}
+  document.getElementById("userid").innerText = id;
+  document.getElementById("username").innerText = name;
+  document.getElementById("usernik").innerText = nik;
+};
 
 function saveToLS(_key, _value) {
   localStorage.setItem(_key, _value);
